6a5cc5b894397341505040ccf354114e977bc344
GoodnessOmo <vondsprit@gmail.com>
2023-08-01 15:25:13 +0100

    app is ready

---
+++ my-server/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const pdfPath = '/HoTH.pdf';
+</script>
+
+<template>
+  <div class="frame">
+    <embed class="pdfFrame" type="application/pdf" :src="pdfPath" width="100%" height="100%" />
+  </div>
+</template>
+
+<style scoped>
+.frame {
+  height: 100vh;
+}
+.pdfFrame {
+  border: none;
+}
+</style>
